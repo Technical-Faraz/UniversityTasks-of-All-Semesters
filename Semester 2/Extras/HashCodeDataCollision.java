830cd0fdf347593b0d6b301fa05d803a8809fee4
Technical-Faraz <60597399+Technical-Faraz@users.noreply.github.com>
2021-02-02 21:36:13 -0800

    Code added
    This code works for only two character string and finds the same hash through brute force attack

--- Semester 2/Extras/HashCodeDataCollision.java
+++ Semester 2/Extras/HashCodeDataCollision.java
@@ -1,1 +1,15 @@
-a
+public class HashCodeDataCollision {
+    //hashCode Data collision
+    public static void main(String[] args) {
+        int generateCode = 2347;
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 256; j++) {
+                if (31 * i + j == generateCode) {
+                    char a = (char)i;
+                    char b = (char)j;
+                    System.out.println(a +" " + b);
+                }
+            }
+        }
+    }
+}
